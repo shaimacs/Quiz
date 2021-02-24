6db046b2fd65228d9b7f4546d9a16e625e2be599
SHAIMA ALSHAMMARY <Shaimacs@yahoo.com>
2021-02-24 16:50:15 +0300

    Deploy

--- quizapp/quizapp/settings.py
+++ quizapp/quizapp/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver.nostatic',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
