dfe45d9446f213f8a3f5a39b9a400a9031290a32
SHAIMA ALSHAMMARY <Shaimacs@yahoo.com>
2021-02-24 17:22:36 +0300

    Deploy

--- quizapp/quizapp/settings.py
+++ quizapp/quizapp/settings.py
@@ -168,5 +168,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
